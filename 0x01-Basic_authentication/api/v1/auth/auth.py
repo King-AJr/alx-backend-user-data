431afdf0ee675b8f41e22a961e28ff8a59595044
King-AJr <talk2king.aj@gmail.com>
2023-09-05 10:03:13 +0000

    class Auth, template for Basic Auth to be implemented

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+module to manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        class to manage the API authentication.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        get authorization header from request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
