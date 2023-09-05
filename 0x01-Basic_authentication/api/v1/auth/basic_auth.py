e4a6547e1afa03d38ad011db007f643d59222372
King-AJr <talk2king.aj@gmail.com>
2023-09-05 20:27:14 +0000

    implement function to convert authorization header from base64 string to utf-8 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ implements methods necessary for Basic Authentication.
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -32,4 +33,21 @@ class BasicAuth(Auth):
         if authorization_header.startswith(base):
             return authorization_header[len(base):]
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            # Attempt to decode the input string as Base64
+            decoded_data = base64.b64decode(base64_authorization_header)
+
+            # If decoding is successful, it's a valid Base64 string
+            return decoded_data.decode('utf-8')
+        except base64.binascii.Error:
+            # If an error is raised, it's not a valid Base64 string
             return None
