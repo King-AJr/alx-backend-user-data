39698bc4d7709eed3b4bab79a45dda58d61c201e
King-AJr <talk2king.aj@gmail.com>
2023-09-05 13:05:12 +0000

    following pycodestyles

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
+module holds class BsicAuth which
+implements methods neceesary to carryout
+Basic Authentication
 """
 
 from api.v1.auth.auth import Auth
+
 
 class BaicAuth(Auth):
     """
