73baa582f77235489f76d85e1c0e94e4a2748c27
King-AJr <talk2king.aj@gmail.com>
2023-09-05 10:55:52 +0000

    implement funcrequire_auth, which will determine if a path requires authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-module to manage the API authentication.
+This module manages API authentication.
 """
 from flask import request
 from typing import List, TypeVar
@@ -8,22 +8,53 @@ from typing import List, TypeVar
 
 class Auth():
     """
-    class to manage the API authentication
+    This class manages API authentication.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        class to manage the API authentication.
+        Checks if authentication is required for a given path.
+
+        Args:
+            path (str): The path to check for authentication.
+            excluded_paths (List[str]): A list of paths that
+            are exempt from authentication.
+
+        Returns:
+            bool: True if authentication is required, False if not.
         """
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        split_path = path.split('/')
+        for ex_path in excluded_paths:
+            ex_paths = ex_path.split('/')
+            if len(split_path) > 3 and len(ex_paths) > 3\
+                    and split_path[3] == ex_paths[3]:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        get authorization header from request
+        Get the authorization header from a Flask request object.
+
+        Args:
+            request (Request): The Flask request object (optional).
+
+        Returns:
+            str: The authorization header.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
+        Get the current user.
+
+        Args:
+            request (Request): The Flask request object (optional).
+
+        Returns:
+            User: The current user.
         """
         return None
