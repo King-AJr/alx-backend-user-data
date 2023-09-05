7762fac4ab00d3ff3118cc680b1d1241555a09bb
King-AJr <talk2king.aj@gmail.com>
2023-09-05 19:59:29 +0000

    implement function to extract authorization header and return the encoded value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-module holds class BsicAuth which
-implements methods neceesary to carryout
-Basic Authentication
+This module contains the `BasicAuth` class, which
+implements methods necessary for Basic Authentication.
 """
 
 from api.v1.auth.auth import Auth
@@ -10,7 +9,27 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """
-    class to implement authentication using
-    basic auth algorithm
+    Class to implement authentication using
+    the Basic Authentication algorithm.
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the base64-encoded portion of an authorization header.
+
+        Args:
+            authorization_header (str): The authorization header string.
+
+        Returns:
+            str: The base64-encoded portion of the header, or None if invalid.
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+
+        base = "Basic "
+        if authorization_header.startswith(base):
+            return authorization_header[len(base):]
+        else:
+            return None
