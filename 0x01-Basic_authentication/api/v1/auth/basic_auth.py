6eec3558cd343093177d464d110edfea2061a315
King-AJr <talk2king.aj@gmail.com>
2023-09-05 20:48:04 +0000

    conforming to pycodestyles

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,19 +35,31 @@ class BasicAuth(Auth):
         else:
             return None
 
-    def decode_base64_authorization_header(self,
-                                           base64_authorization_header:
-                                           str) -> str:
-        if base64_authorization_header is None or not isinstance(
-                base64_authorization_header, str):
-            return None
 
-        try:
-            # Attempt to decode the input string as Base64
-            decoded_data = base64.b64decode(base64_authorization_header)
+def decode_base64_authorization_header(self,
+                                       base64_authorization_header:
+                                       str) -> str:
+    """
+    Decode a Base64-encoded authorization header and return the decoded string.
 
-            # If decoding is successful, it's a valid Base64 string
-            return decoded_data.decode('utf-8')
-        except base64.binascii.Error:
-            # If an error is raised, it's not a valid Base64 string
-            return None
+    Args:
+        base64_authorization_header (str): The Base64-encoded authorization
+        header string.
+
+    Returns:
+        str: The decoded string if successful, or None if it's not a valid
+        Base64 string.
+    """
+    if base64_authorization_header is None or not isinstance(
+                    base64_authorization_header, str):
+        return None
+
+    try:
+        # Attempt to decode the input string as Base64
+        decoded_data = base64.b64decode(base64_authorization_header)
+
+        # If decoding is successful, it's a valid Base64 string
+        return decoded_data.decode('utf-8')
+    except base64.binascii.Error:
+        # If an error is raised, it's not a valid Base64 string
+        return None
