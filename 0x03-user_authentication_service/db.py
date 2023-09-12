8d65be19b51a17d1fe96c5e8f4d25b091509a014
King-AJr <talk2king.aj@gmail.com>
2023-09-12 17:58:45 +0100

    comment function add_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,24 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email address of the new user.
+            hashed_password (str): The hashed password of the new user.
+
+        Returns:
+            User: The newly created User object.
+        """
+        # Create a new User object with the provided email and hashed_password
         new_user = User(email=email, hashed_password=hashed_password)
+
+        # Add the new_user object to the session (pending database insertion)
         self._session.add(new_user)
+
+        # Commit the transaction to persist the new user to the database
         self._session.commit()
+
+        # Return the newly created User object
         return new_user
