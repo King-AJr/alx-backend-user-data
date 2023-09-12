3be295b9eaac0b5de3e40498cc213ac75376fa49
King-AJr <talk2king.aj@gmail.com>
2023-09-12 17:54:26 +0100

    pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-
-from user import Base
+from user import User, Base
 
 
 class DB:
@@ -29,7 +27,6 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
 
     def add_user(self, email: str, hashed_password: str) -> User:
         new_user = User(email=email, hashed_password=hashed_password)
