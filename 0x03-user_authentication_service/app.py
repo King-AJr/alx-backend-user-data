2b9c896cea5aae93ea1944450be517a2864e431e
King-AJr <talk2king.aj@gmail.com>
2023-09-13 09:34:22 +0100

    Register a user based on form data and handle registration errors.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,10 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -13,6 +15,40 @@ def hello():
     return french message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    Register a user based on form data and handle registration errors.
+
+    This function extracts 'email' and 'password' from a form submitted
+    in a request, attempts to register a new user with the provided email
+    and password, and handles registration errors, such as duplicate email
+    addresses.
+
+    Returns:
+        JSON response with appropriate status code and messages.
+    """
+    # Extract 'email' and 'password' from the submitted form data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Print the extracted email and password for debugging purposes
+    print(email, password)
+
+    try:
+        # Attempt to register a new user with the provided email and password
+        AUTH.register_user(email, password)
+
+        # If registration is successful, return a JSON response with success
+        # message
+        return jsonify({"email": "{}".format(email),
+                        "message": "user created"}), 200
+    except ValueError:
+        # If a ValueError is raised (e.g., due to a duplicate email), return an
+        # error response
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
